34976a64c1ecc33caef46b5f7e6fe5e0b453b038
vtayal2405 <126266010+vtayal2405@users.noreply.github.com>
2024-01-31 19:45:32 +0530

    Update ques1.cpp

--- Week1/ques1.cpp
+++ Week1/ques1.cpp
@@ -1,1 +1,31 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+  int arr[25],n,i,x,f=0;
+  cout<<"Enter no. of elements in array : ";
+  cin>>n;
+  cout<<"Enter the positive numbers in array : "<<endl;
+  for(i=0;i<n;i++)
+    {
+      cin>>arr[i];
+    }
+  cout<<"Enter a number to be searched :";
+  cin>>x;
+  for(i=0;i<n;i++)
+    {
+      if(x==arr[i])
+      {
+        f=1;
+        break;
+      }
+    }
+    if(f==1)
+    {
+        cout<<"Present "<<i+1<<endl;
+    }
+    if(f==0)
+    {
+        cout<<"Not Present "<<i+1<<endl;
+    }
+}
