12fd461f8056b5f4724239ebd76ad2f2fdcfcb43
vtayal2405 <126266010+vtayal2405@users.noreply.github.com>
2024-02-01 09:35:05 +0530

    Update ques3.cpp

--- Week1/ques3.cpp
+++ Week1/ques3.cpp
@@ -1,1 +1,44 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int arr[25],n,i,j,k,f=0,a,b;
+    cout<<"Enter size of array : ";
+    cin>>n;
+    cout<<"Enter numbers in sorted order in array :"<<endl;
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Enter the number to be searched : ";
+    cin>>k;
+    if(arr[0]==k)
+    {
+        cout<<"Present";
+    }
+    i=1;
+    while(i<n && arr[i]<k)
+    {
+        i*=2;
+    }
+    a=i/2;
+    b=min(i,n-1);
+    for(j=a;j<=b;j++)
+    {
+        if(arr[b]==k)
+        {
+            f=1;
+            break;
+        }
+    }
+    if(f==1)
+    {
+        cout<<"Present";
+    }
+    else
+    {
+        cout<<"Not present";
+    }
+}
 
