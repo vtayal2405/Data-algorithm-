3fb9ce40ab1733790166fe74bfb7b2801bade9ea
vtayal2405 <126266010+vtayal2405@users.noreply.github.com>
2024-02-07 21:58:10 +0530

    Update ques3.cpp

--- Week2/ques3.cpp
+++ Week2/ques3.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[25],t,n,i,j,k,l,key,c;
+    cout<<"Enter no. of test cases : ";
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        cout<<"Enter size of array : ";
+        cin>>n;
+        cout<<"Enter elements in array : ";
+        for(j=0;j<n;j++)
+        {
+            cin>>arr[j];
+        }
+        cout<<"Enter a key : ";
+        cin>>key;
+        c=0;
+        for(k=0;k<n;k++)
+        {
+            for(l=k+1;l<n;l++)
+            {
+                if(abs(arr[k]-arr[l])==key)
+                c++;
+            }
+        }
+        cout<<c;
+    }
+}
 
