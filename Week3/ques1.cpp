28a26cd2e1e8c4e4df145694ef9f64229834b982
vtayal2405 <126266010+vtayal2405@users.noreply.github.com>
2024-02-08 09:48:57 +0530

    Update ques1.cpp

--- Week3/ques1.cpp
+++ Week3/ques1.cpp
@@ -1,1 +1,34 @@
-
+#include<iostream>
+using namespace std;
+void insertion(int arr[],int n,)
+{
+    int t=0,i,j;
+    for(i=1;i<n;i++)
+    {
+        j=i-1;
+        while(j>=0 && arr[j]>t)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=t;
+        
+    }
+}
+int main()
+{
+    int arr[25],n,i;
+    cout<<"Enter no. of test cases : ";
+    cin>>t;
+    for(l=0;l<t;l++)
+    {
+        cout<<"Enter size of array : ";
+        cin>>n;
+        cout<<"Enter elements in array : "<<endl;
+        for(i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        insertion(arr,n);
+    }
+}
