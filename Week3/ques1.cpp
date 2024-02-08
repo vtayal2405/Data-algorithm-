869692959f32f7287eeb7964279062b71db71ee0
vtayal2405 <126266010+vtayal2405@users.noreply.github.com>
2024-02-08 22:48:27 +0530

    Update ques1.cpp

--- Week3/ques1.cpp
+++ Week3/ques1.cpp
@@ -1,23 +1,31 @@
 #include<iostream>
 using namespace std;
-void insertion(int arr[],int n,)
+void insertion(int arr[],int n)
 {
-    int t=0,i,j;
+    int t=0,i,j,c=0,b=0;
     for(i=1;i<n;i++)
     {
+        t=arr[i];
         j=i-1;
+        b++;
         while(j>=0 && arr[j]>t)
         {
+            c++;
+            b++;
             arr[j+1]=arr[j];
             j--;
         }
         arr[j+1]=t;
-        
     }
+    for(i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\ncompanisons = "<<c<<"\nshifts = "<<b<<endl;
 }
 int main()
 {
-    int arr[25],n,i;
+    int arr[25],n,i,t,l;
     cout<<"Enter no. of test cases : ";
     cin>>t;
     for(l=0;l<t;l++)
